b15e59cd66ae68ca305649af7c47d15d5c902157
Jean Peel <Jean.Peel.246@gmail.com>
2020-06-16 22:16:33 -0600

    edited items

--- bamazon.sql
+++ bamazon.sql
@@ -7,24 +7,26 @@ use Bamazon;
 CREATE TABLE products(
     --Not null because item id should not be zero.
     item_id INT AUTOINCREMENT NOT NULL,
-    product_name VARCHAR(30) NOT NULL,
-    department_name VARCHAR(30) NOT NULL,
+    product_name VARCHAR(100) NOT NULL,
+    department_name VARCHAR(100) NOT NULL,
     price DECIMAL(10, 2) NOT NULL,
     stock_quantity INT(10) NOT NULL,
-PRIMARY KEY(item_id)
+    PRIMARY KEY(item_id)
 );
 
 SELECT * FROM products
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 
-VALUES ("Uncharted 4", "Video Games", 49.95, 150),
-  ("DOOM", "Video Games", 59.99, 200),
-  ("Crate of Spam", "Food and Drink", 24.50, 50),
-  ("Cool Shades", "Apparel", 75.00, 5),
-  ("Worn Denim Jeans", "Apparel", 54.25, 35),
-  ("Survival Towel", "Necessities", 42.42, 42),
-  ("Bill and Ted's Excellent Adventure", "Films", 15.00, 25),
-  ("Mad Max: Fury Road", "Films", 25.50, 57),
-  ("Monopoly", "Board Games", 30.50, 35),
-  ("Yahtzee", "Board Games", 19.95, 23);
+VALUES ('SmartyPants Kids Multivitamin', 'Health', 14.63, 261),
+('Heroes: The Greek Myths Reimagined',  'Audiobook', 9.99, 8),
+('Friends: The Complete Series Collection', 'DVD', 59.99, 121),
+('RIOGOO Pet Heating Pad', 'Pets', 23.99, 86),
+('Treasure X Aliens Ultimate Dissection Kit', 'Toys', 24.86, 232),
+('Everyone Is Welcome Here Pride Banner', 'Handmade', 19.95, 151),
+('Coleman 0°F Mummy Sleeping Bag', 'Outdoors', 49.99, 59),
+('Viva Naturals Ground Flax Seed', 'Grocery', 16.98, 86),
+('Mini Projector - 3600L Hompow', 'Electronics', 138.99, 29),
+('Sterling by Music Man, Bass Guitar', 327.13, 12),
+('New Apple MacBook Air', 'Computers', 899.00, 101),
+('Vivere Double Cotton Hammock', 'Garden', 329.87, 32);
